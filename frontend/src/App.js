262592080697665818884e881aef3a0570b50858
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-07-11 08:33:39 +0400

    starting frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div>Proshop</div>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
