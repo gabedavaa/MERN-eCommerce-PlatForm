92964c5ed85060e213565b4b7bcae5a3cc5a15d1
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-07-25 07:48:08 +0400

    frontend authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
-import { Container } from "react-bootstrap";
-import { Outlet } from "react-router-dom";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <>
       <Header />
-      <main className="py-3">
+      <main className='py-3'>
         <Container>
           <Outlet />
         </Container>
       </main>
       <Footer />
+      <ToastContainer />
     </>
   );
 };
